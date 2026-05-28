3ae6887c2f19c9e8cac26584b6417af65c1bac6b
Toan Bach <toan@wecheer.me>
2026-05-28 15:18:26 +0700

    2.5. Add STDIO

--- src/OrderManagement.McpServer/Program.cs
+++ src/OrderManagement.McpServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OrderManagement.Application;
 using OrderManagement.Application.Common.Interfaces;
 using OrderManagement.Application.Contracts;
@@ -11,12 +12,11 @@ using OrderManagement.McpServer.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Tắt console logger mặc định
-//builder.Logging.ClearProviders();
+builder.Logging.ClearProviders();
 
 // ✅ Tái sử dụng DI từ Web API — không viết lại
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration, builder.Environment);
-
 
 // ✅ Đăng ký MCP Server với stdio transport
 builder.Services
@@ -26,7 +26,7 @@ builder.Services
     .WithResources<SystemInfoResource>()
     .WithPromptsFromAssembly()    // scan tất cả [McpServerPrompt] trong assembly
                                   //.WithResourcesFromAssembly()   // scan tất cả [McpServerResource] trong assembly
-    ///.WithStdioServerTransport()   // stdio: dùng console in/out
+    .WithStdioServerTransport()   // stdio: dùng console in/out
     .WithToolsFromAssembly();     // scan tất cả [McpServerTool] trong assembly
 
 builder.Services.AddHttpContextAccessor();
